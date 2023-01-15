91ffbc6a9befed770b1d1279c36c7ee997d85583
Nuraiym Obozkanova <nuraika.obozkanova@gmail.com>
2023-01-15 15:00:58 +0600

    own user

--- trello/models.py
+++ trello/models.py
@@ -5,16 +5,76 @@ from datetime import date
 from config import settings
 from django.contrib.auth.models import AbstractUser
 from django_resized import ResizedImageField
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin)
+
+from django.db import models
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise TypeError('Users should have a username')
+        if email is None:
+            raise TypeError('Users should have a Email')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        if password is None:
+            raise TypeError('Password should not be none')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
 
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
+AUTH_PROVIDERS = {'facebook': 'facebook', 'google': 'google',
+                  'twitter': 'twitter', 'email': 'email'}
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ("username", )
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True, db_index=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    auth_provider = models.CharField(
+        max_length=255, blank=False,
+        null=False, default=AUTH_PROVIDERS.get('email'))
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
+
+
+# class CustomUser(AbstractUser):
+#     email = models.EmailField(unique=True)
+#
+#     USERNAME_FIELD = "email"
+#     REQUIRED_FIELDS = ("username", )
+#
+#     def __str__(self):
+#         return self.username
 
 
 class MemberDesk(models.Model):
